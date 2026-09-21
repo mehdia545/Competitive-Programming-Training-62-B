bed8dc1b2d8a8e6da81176dc879512128f239a2e
Mehdia 251-115-087 <mehdiaalam2021@gmail.com>
2026-09-21 13:39:13 +0600

    Update problem2.cpp

--- Day 3/problem2.cpp
+++ Day 3/problem2.cpp
@@ -10,18 +10,17 @@ int main() {
     v.push_back(50);
     int n = v.size();
     
-    // Swap elements from the outside going inwards using two pointers
+   
     for(int i = 0; i < n / 2; i++) {
         int temp = v[i];
         v[i] = v[n - 1 - i];
         v[n - 1 - i] = temp;
-        // Tip: You can also just use the built-in swap(v[i], v[n - 1 - i]);
+        
     }
 		 for (int i=0;i < n;i++)
     {
 			    cout << v[i] << endl;
     }
     
-    // It is now reversed!
     return 0;
 }
